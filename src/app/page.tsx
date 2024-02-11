b50834d91c2470d34dc42dbd8aa1bf2d55f922c4
Joe Monaco <jmonaco0506@gmail.com>
2024-02-11 16:48:29 -0500

    remove weird character

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,12 @@ import { Link } from "lucide-react";
 export default function Home() {
   return (
     <>
-      <h1 className="text-3xl font-bold text-center my-8">Erika's Recipes</h1>
-      <h2 className="text-xl text-center my-8">
-        A place for all my recipes curated by me! Click{" "}
-        <a href="/recipes" className="text-pink-500">
+      <h1 className='text-3xl font-bold text-center my-8'>Erika's Recipes</h1>
+      <h2 className='text-xl text-center my-8'>
+        A place for all my recipes curated by me! Click
+        <a href='/recipes' className='text-pink-500'>
           Here
-        </a>{" "}
+        </a>
         to see them all!
       </h2>
     </>
